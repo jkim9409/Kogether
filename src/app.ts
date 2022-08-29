7adee69e3e51b73d177e33aa28b109868de574dc
jkim9409 <92039854+jkim9409@users.noreply.github.com>
2022-08-29 23:25:19 +0900

    add graphql basic setting

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,53 @@
 import express, {Request, Response, NextFunction, Application, ErrorRequestHandler} from "express";
-import {Server} from 'http'
+import {Server} from "http";
+import { buildSchema } from 'type-graphql';
+import { ApolloServer} from 'apollo-server-express';
 import createHttpError from "http-errors";
+import { UserResolver } from './graphql/user'
+import "reflect-metadata";
 
-const PORT: Number = Number(process.env.PORT) || 3000;
-const app: Application = express()
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('hello from ts app');
-});
-
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-    next();
-});
-
-const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) =>{
-    res.status(err.status || 500)
-    res.send({
-        status: err.staus || 500,
-        message: err.message
-    })
-};
+async function createApolloServer(port: number) {
+    const app = express()
+  
+    const schema = await buildSchema({ resolvers: [UserResolver] }) // 스키마로 변환!
+    const server = new ApolloServer({ schema })
+  
+    await server.start()
+    server.applyMiddleware({ app })
+  
+    await new Promise<void>((resolve) => app.listen(port, resolve))
+    console.log(`Server running on :${port}`)
+  
+    return { server, app }
 
 
-app.use(errorHandler);
+  }
+
+  createApolloServer(3000);
 
 
-const server: Server = app.listen(PORT, ()=> console.log('Listening to port 3000'))
+// const PORT: Number = Number(process.env.PORT) || 3000;
+// const app: Application = express()
+
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//     res.send('hello from ts app');
+// });
+
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     next();
+// });
+
+// const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) =>{
+//     res.status(err.status || 500)
+//     res.send({
+//         status: err.staus || 500,
+//         message: err.message
+//     })
+// };
+
+
+// app.use(errorHandler);
+
+
+// const server: Server = app.listen(PORT, ()=> console.log('Listening to port 3000'))
